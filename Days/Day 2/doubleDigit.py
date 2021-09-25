047ce6239a39eb80cfaf02e346a84ed7a701f94b
jenkins2099 <91299789+jenkins2099@users.noreply.github.com>
2021-09-24 17:50:55 -0700

    Create doubleDigit.py

---
+++ Days/Day 2/doubleDigit.py
@@ -0,0 +1,6 @@
+#Enter two digit number
+two_digit_number = input("Type a two digit number: ")
+
+#Add the digits that constitute the two digit number 
+print(int(two_digit_number[0])+int(two_digit_number[1]))
+
