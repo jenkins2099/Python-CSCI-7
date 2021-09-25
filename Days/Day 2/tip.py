29b42ddb57185b09046e6162d1236f921dcf4767
jenkins2099 <91299789+jenkins2099@users.noreply.github.com>
2021-09-24 19:48:13 -0700

    Delete tip.py

--- Days/Day 2/tip.py
+++
@@ -1,5 +0,0 @@
-total_bill=float(input("What was the total bill? "))
-bill_split=float(input("How many people are there to split the bill? "))
-tip_percentage=float(input("What percentage of tip would you like to give? 10, 12, or 15? "))/100
-tip_total=round(float((total_bill/bill_split)*tip_percentage),2)
-print(f"Each person should pay ${tip_total}")
