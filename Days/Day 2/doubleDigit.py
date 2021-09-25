b36096755afb41985bc24dede64081d3b2c740f5
jenkins2099 <91299789+jenkins2099@users.noreply.github.com>
2021-09-24 20:03:20 -0700

    Update doubleDigit.py

--- Days/Day 2/doubleDigit.py
+++ Days/Day 2/doubleDigit.py
@@ -1,6 +1,5 @@
-#Enter two digit number
+#Take a two digit number and add the digits that constitute it
 two_digit_number = input("Type a two digit number: ")
 
-#Add the digits that constitute the two digit number 
 print(int(two_digit_number[0])+int(two_digit_number[1]))
 
